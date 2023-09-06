1a03dd3bb3a4bb5ad123f84a55823388f68a6e9e
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-09-06 13:36:18 +0300

    initial commut

---
+++ src/main/java/com/example/wayz/WayzApplication.java
@@ -0,0 +1,13 @@
+package com.example.wayz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WayzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wayz/WayzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wayz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WayzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
