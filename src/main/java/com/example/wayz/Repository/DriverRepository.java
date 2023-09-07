f40d76e20153503c6c8ab8e23aafb3a576115e0c
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-09-07 04:48:15 +0300

    add findDriverById #8

--- src/main/java/com/example/wayz/Repository/DriverRepository.java
+++ src/main/java/com/example/wayz/Repository/DriverRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DriverRepository extends JpaRepository<Driver,Integer> {
+
+    Driver findDriverById(Integer id);
+
 }
