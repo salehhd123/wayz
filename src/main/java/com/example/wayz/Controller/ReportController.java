9e572805f233096b45e45af12d0ed192f504cc64
saleh <saleh@example.com>
2023-09-07 10:49:15 +0300

    controll report #18

--- src/main/java/com/example/wayz/Controller/ReportController.java
+++ src/main/java/com/example/wayz/Controller/ReportController.java
@@ -1,10 +1,14 @@
 package com.example.wayz.Controller;
 
+import com.example.wayz.Api.ApiResponse.ApiResponse;
+import com.example.wayz.DTO.ReportDto;
+import com.example.wayz.Model.User;
 import com.example.wayz.Repository.ReportRepository;
 import com.example.wayz.Service.ReportService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,8 +17,41 @@ public class ReportController {
 
     private final ReportService reportService;
 
+    @GetMapping("/get-all")
+    public ResponseEntity getAll(){
+        return ResponseEntity.status(200).body(reportService.getALlReport());
+    }
+
+    @GetMapping("/get-all-pending")
+    public ResponseEntity getPending(){
+        return ResponseEntity.status(200).body(reportService.getAllReportPending());
+    }
+
+    @PostMapping("/add-report/{id}")
+    public ResponseEntity addReport(@AuthenticationPrincipal User user, @PathVariable Integer id, @RequestBody ReportDto reportDto){
+       reportService.addReport(user.getId(),id,reportDto);
+        return ResponseEntity.status(200).body(new ApiResponse("the report added"));
+    }
 
 
+    @DeleteMapping("/delete-report/{id}")
+    public ResponseEntity deleteReport(Integer id){
+        reportService.delete(id);
+        return ResponseEntity.status(200).body(new ApiResponse("the report deleted"));
+    }
 
+
+    @PutMapping("/ignore/{id}")
+        public ResponseEntity ignore(@AuthenticationPrincipal User user, @PathVariable Integer id) {
+        reportService.ignoreReport(user.getId(),id);
+        return ResponseEntity.status(200).body(new ApiResponse("the report change to ignore"));
+    }
+
+
+    @PutMapping("/approve/{id}")
+    public ResponseEntity approve(@AuthenticationPrincipal User user, @PathVariable Integer id){
+        reportService.approveReport(user.getId(),id);
+        return ResponseEntity.status(200).body(new ApiResponse("the report change to approve"));
+    }
 
 }
