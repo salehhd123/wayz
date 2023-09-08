d0cfb85630e032f29663d004ad904fa6bf1d8c03
saleh <saleh@example.com>
2023-09-08 16:22:15 +0300

    add query the will list the top ten driver for how many trips they did #10

---
+++ src/main/java/com/example/wayz/Model/TopDrivers.java
@@ -0,0 +1,14 @@
+package com.example.wayz.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TopDrivers {
+
+    private Driver driver;
+
+    private Integer tripsCount;
+
+}
--- src/main/java/com/example/wayz/Repository/DriverTripsRepository.java
+++ src/main/java/com/example/wayz/Repository/DriverTripsRepository.java
@@ -1,7 +1,11 @@
 package com.example.wayz.Repository;
 
+import com.example.wayz.Model.Driver;
 import com.example.wayz.Model.DriverTrips;
+import com.example.wayz.Model.Student;
+import com.example.wayz.Model.TopDrivers;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +15,9 @@ public interface DriverTripsRepository extends JpaRepository<DriverTrips, Intege
 
     // TODO: need testing findAllByDriverId
     List<DriverTrips> findAllByDriverId(Integer driverId);
+
+
+    @Query("select c.driver, count (c) from driver_trips c order by count(c.driver.id) DESC limit 10")
+    List<TopDrivers> findTheMostDriver();
 
 }
